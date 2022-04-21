484e6524dde71a01614e48867400a96ae6f3dc67
Dariusz Knap <50834371+ScarletDesigner@users.noreply.github.com>
2022-04-21 20:33:31 +0200

    7_11 Scene management Avoiding the singleton pattern

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,25 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
-public class Portal : MonoBehaviour
+namespace RPG.SceneManagement
 {
-    [SerializeField] int sceneToLoad = -1;
-    
-    private void OnTriggerEnter(Collider other)
+    public class Portal : MonoBehaviour
     {
-        if (other.tag == "Player")
+        enum DestinationIdentifier
         {
-            StartCoroutine(Transition());
+            first, second, third, forth
+        }
+
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+        [SerializeField] DestinationIdentifier currentLocation;
+        [SerializeField] DestinationIdentifier destination;
+        [SerializeField] float fadeOutTime = 0.5f;
+        [SerializeField] float fadeInTime = 1f;
+        [SerializeField] float fadeWaitTime = 0.5f;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                StartCoroutine(Transition());
+            }
+        }
+
+        private IEnumerator Transition()
+        {
+            if (sceneToLoad < 0)
+            {
+                Debug.LogError("Scene to laod not set.");
+                yield break;
+            }
+
+            DontDestroyOnLoad(this.gameObject);
+            Fader fader = FindObjectOfType<Fader>();
+            yield return fader.FadeOut(fadeOutTime);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            yield return new WaitForSeconds(fadeWaitTime);
+
+            yield return fader.FadeIn(fadeInTime);
+
+            Destroy(this.gameObject);
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (this.currentLocation != portal.destination) continue;
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.transform.position);
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
         }
     }
 
-    private IEnumerator Transition()
-    {
-        DontDestroyOnLoad(this.gameObject);
-        yield return SceneManager.LoadSceneAsync(sceneToLoad);
-        print("Scene Loaded");
-        Destroy(this.gameObject);
-    }
 }
